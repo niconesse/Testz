54fa26b2046775a70e02712cac2f38fc83508426
Nicolas Marinesse <SCJQ7401@orange.com>
2019-04-22 00:34:11 +0200

    rajout de la premiere version de Main.java

--- src/com/psg/nm/Main.java
+++ src/com/psg/nm/Main.java
@@ -3,6 +3,14 @@ package com.psg.nm;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] tirage = new int[5];
+        for (int i = 0; i < 5; i++) {
+            tirage[i] = (int) (Math.random() * 6 + 1);
+        }
+        java.util.Arrays.sort(tirage);
+        for (int i = 0; i < 5; i++) {
+            System.out.print(tirage[i] + " ");
+        }
+        System.out.println();
     }
 }
